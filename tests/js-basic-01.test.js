a4306913f0326705e0ac484521499fdf1a637f59
zieraell <roobydemongirl@gmail.com>
2023-09-21 01:55:28 -0400

    Part 1
    Completed Test Files 1 and 2

--- tests/js-basic-01.test.js
+++ tests/js-basic-01.test.js
@@ -24,7 +24,6 @@
 ? In Node, the scope of a global variable is the file that it is defined in.
 ?
 
-
 ? All of the variables and constants in this file are global variables.
 ? This is why we can access them anywhere after they are declared.
 ? We'll use this behavior to reuse some variables and constants multiple times
@@ -44,6 +43,8 @@ TODO: Use the let keyword to declare a variable called myName and make sure you
 TODO: DO NOT assign it a value.
 */
 // * Add your code BELOW here
+
+let myName;
 
 // * Don't mess with the test code below here!
 
@@ -68,6 +69,8 @@ TODO: number literal value of integer 22.
 
 // * Add your code BELOW here
 
+let age = 22;
+
 // * Don't mess with the test code below here!
 
 test('age', () => {
@@ -89,6 +92,8 @@ TODO: the number literal value 7.
 */
 
 // * Add your code BELOW here
+
+const daysInWeek = 7;
 
 // * Don't mess with the test code below here!
 
@@ -129,6 +134,9 @@ TODO: of one variable to a different variable.
 
 // * Add your code BELOW here
 
+let myWorkDays;
+myWorkDays = daysInWeek;
+
 // * Don't mess with the test code below here!
 
 test('myWorkDays', () => {
@@ -160,6 +168,10 @@ TODO: (using the * operator for multiplication).
 
 // * Add your code BELOW here
 
+let oops;
+let sillyMath;
+sillyMath = oops * 2;
+
 // * Don't mess with the test code below here!
 
 test('sillyMath', () => {
@@ -187,6 +199,10 @@ TODO: In the second line, assign sillyString to 'Hello, ' and concatenate oops t
 
 // * Add your code BELOW here
 
+let sillyString;
+sillyString = 'Hello, ';
+sillyString += oops;
+
 // * Don't mess with the test code below here!
 
 test('sillyString', () => {
@@ -209,6 +225,8 @@ TODO: keyword and assign it a value.
 // a variable (let) named year and set it to 2022
 // * Add your code BELOW here
 
+const year = 2022;
+
 // * Don't mess with the test code below here!
 test('year', () => {
   expect(year).toBe(2022);
@@ -219,6 +237,8 @@ test('year', () => {
 
 // * Add your code BELOW here
 
+let firstName = 'John';
+
 // * Don't mess with the test code below here!'
 test('firstName', () => {
   expect(firstName).toBe('John');
@@ -226,6 +246,8 @@ test('firstName', () => {
 });
 
 // a constant called taxRate and set it to 0.2
+
+const taxRate = 0.2;
 
 // * Add your code BELOW here
 
--- tests/js-basic-02.test.js
+++ tests/js-basic-02.test.js
@@ -38,6 +38,8 @@ TODO: the string literal 'JavaScript is Awesome!'.
 
 // * Add your code BELOW here
 
+let myClass = 'JavaScript is Awesome!';
+
 // * Don't mess with the test code below here!
 
 test('myClass', () => {
@@ -58,6 +60,8 @@ TODO: Remember to use single-quotes for the empty string.
 */
 
 // * Add your code BELOW here
+
+let emptyString = '';
 
 // * Don't mess with the test code below here!
 
@@ -83,6 +87,8 @@ TODO: string with exactly 6 alphanumeric characters in it.
 
 // * Add your code BELOW here
 
+let petName = 'Charli';
+
 // * Don't mess with the test code below here!
 
 test('petName', () => {
@@ -104,6 +110,8 @@ TODO: assign it any word that starts with a capital A.
 */
 
 // * Add your code BELOW here
+
+let firstLetterIsA = 'Alpine';
 
 // * Don't mess with the test code below here!
 
@@ -131,6 +139,8 @@ TODO: and two is not a space but an escaped horizontal tab character.
 
 // * Add your code BELOW here
 
+let counting = 'one\ttwo';
+
 // * Don't mess with the test code below here!
 
 test('counting', () => {
@@ -154,6 +164,8 @@ TODO: assigned variable petName (from Skill 3).
 */
 
 // * Add your code BELOW here
+
+let mySentence = 'Hello, ' + petName;
 
 // * Don't mess with the test code below here!
 
@@ -183,6 +195,8 @@ TODO: for a JavaScript expression inside a template literal.
 
 // * Add your code BELOW here
 
+let helloPet = `Hello, ${petName}`;
+
 // * Don't mess with the test code below here!
 
 test('helloPet', () => {
@@ -211,6 +225,9 @@ TODO: In line 2: Reassign thisMonth to the string literal 'September'.
 */
 
 // * Add your code BELOW here
+
+let thisMonth = 'I dunno!';
+thisMonth = 'September';
 
 // * Don't mess with the test code below here!
 
@@ -246,6 +263,11 @@ TODO: In the below area, you will practice making template literals
 
 // * Add your code BELOW here
 
+const year = 2001;
+const firstName = 'John';
+const lastName = 'Smith';
+let message = `Hi, my name is ${firstName} ${lastName} and I was born in ${year}`;
+
 // * Don't mess with the test code below here!
 
 test('creating a message', () => {
@@ -265,6 +287,10 @@ test('creating a message', () => {
 
 // * Add your code BELOW here
 
+const tax = 8.25;
+const price = 100;
+let total = `Your total including tax is $${price + tax}`;
+
 // * Don't mess with the test code below here!
 
 test('creating a total', () => {
@@ -278,6 +304,10 @@ test('creating a total', () => {
 // TODO: such that you get the string - '<h1>JavaScript</h1><h2>HTML</h2><p>This is a paragraph</p>'
 
 // * Add your code BELOW here
+
+let heading1 = 'JavaScript';
+let heading2 = 'HTML';
+let html = `<h1>${heading1}</h1><h2>${heading2}</h2><p>This is a paragraph</p>`;
 
 // * Don't mess with the test code below here!
 
